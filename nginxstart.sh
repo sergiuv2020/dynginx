c70640fa00037674e6e6fdf8612fa71be161c887
Sergiu-Alexandru Vidrascu <vsergiu@hotmail.com>
2016-10-21 12:46:28 +0300

    Add location rewrite and set headers

--- nginxstart.sh
+++ nginxstart.sh
@@ -7,7 +7,7 @@ IFS=',' read -r -a array <<< "$FULLENV"
 for i in "${array[@]}"
 do
    path=$(sed -e "s/:.*//g" <<< "$i")
-   locations="$locations\n\t\t\tlocation /$path {\n\t\t\t\tproxy_pass http://upstream$path/$path;\n\t\t\t}\n"
+   locations="$locations\n\t\t\tlocation /$path {\n\t\t\t\tproxy_pass http://upstream$path/$path;\nproxy_set_header Host \$host;\nproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\nproxy_set_header X-Forwarded-Server \$host;\nproxy_set_header X-Real-IP \$remote_addr;\n\t\t\t}\n"
    i=$(sed -e "s/$path://g" <<< "$i")
    IFS='/' read -r -a backends <<< "$i"
    for i in "${backends[@]}"
@@ -20,6 +20,7 @@ done
 locations="\t\t$locations\n\t\t}\n"
 upstreams="\t\t$upstreams\n}"
 sed -ie "s/listen.*/listen $NGINX_PORT;/g" /etc/nginx/nginx.conf
+sed -ie "s/rewrite.*/rewrite ^/$ /$NGINX_REWRITE_PATH;/g" /etc/nginx/nginx.conf
 echo -en $locations >> /etc/nginx/nginx.conf
 echo -en $upstreams >> /etc/nginx/nginx.conf
 
